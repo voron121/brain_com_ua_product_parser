27950e527bf1416ad6862a8f6341eb02541dfcbb
Вадим V <phogame121@gmail.com>
2022-06-03 00:02:28 +0300

    Add offers into price list generator class

--- classes/PriceListGenerator.php
+++ classes/PriceListGenerator.php
@@ -10,6 +10,8 @@ class PriceListGenerator
 
     const PRICE_LIST_PATH = __DIR__ . '/../pricelist/';
 
+    const CURRENCY_CODE = 'UAH';
+
     protected $db;
 
     protected $dom;
@@ -46,10 +48,10 @@ class PriceListGenerator
                          articul, 
                          weight, 
                          price_uah, 
-                         full_image, 
+                         full_image,
+                         vendorID,
                          options 
-                    FROM products
-                    LIMIT 0, 2';
+                    FROM products';
         $stmt = $this->db->query($query);
         return $stmt->fetchAll();
     }
@@ -85,7 +87,7 @@ class PriceListGenerator
     private function getCurrenciesNode(): \DOMElement
     {
         $currency = $this->dom->createElement('currency');
-        $currency->setAttribute('id', 'UAH');
+        $currency->setAttribute('id', self::CURRENCY_CODE);
         $currency->setAttribute('rate', '1');
         $currencies = $this->dom->createElement('currencies');
         $currencies->appendChild($currency);
@@ -110,12 +112,62 @@ class PriceListGenerator
         return $categories;
     }
 
+    /**
+     * @param $product
+     * @return \DOMElement
+     * @throws \DOMException
+     */
+    private function getOfferNode($product): \DOMElement
+    {
+        $offer = $this->dom->createElement('offer');
+        $offer->setAttribute('id', $product->productID);
+        $offer->setAttribute('available', $product->is_archive === 'no' ? 'true' : 'false');
+        $offer->appendChild($this->dom->createElement('price', $product->price_uah));
+        $offer->appendChild($this->dom->createElement('description', strip_tags(trim($product->description))));
+        $offer->appendChild($this->dom->createElement('currencyId', self::CURRENCY_CODE));
+        $offer->appendChild($this->dom->createElement('categoryId', $product->categoryID));
+        $offer->appendChild($this->dom->createElement('picture', $product->full_image));
+        $offer->appendChild($this->dom->createElement('name', strip_tags(trim($product->name))));
+        $offer->appendChild($this->dom->createElement('vendorCode', $product->vendorID));
+        // HARDCODE: i dont'now from what field get this value? so set 100
+        $offer->appendChild($this->dom->createElement('stock_quantity', 100));
+        // $offer->appendChild($this->dom->createElement('brend', ''));
+        // Add params if they exist
+        if (!is_null($product->options)) {
+            $options = json_decode($product->options);
+            foreach ($options as $option) {
+                $param = $this->dom->createElement('param', $option->value);
+                $param->setAttribute('name', $option->name);
+                $offer->appendChild($param);
+            }
+        }
+        return $offer;
+    }
+
+    /**
+     * @return \DOMElement
+     * @throws \DOMException
+     */
+    private function getOffersNode(): \DOMElement
+    {
+        $offersNode = $this->dom->createElement('offers');
+        foreach ($this->getProducts() as $product) {
+            $offersNode->appendChild($this->getOfferNode($product));
+        }
+        return $offersNode;
+    }
+
+    /**
+     * @return void
+     * @throws \DOMException
+     */
     private function createPriceList()
     {
         $priceList = $this->getYmlСatalogNode();
         $shopNode = $this->getShopNode();
         $shopNode->appendChild($this->getCurrenciesNode());
         $shopNode->appendChild($this->getCategoriesNode());
+        $shopNode->appendChild($this->getOffersNode());
         $priceList->appendChild($shopNode);
         $this->dom->appendChild($priceList);
     }
@@ -126,8 +178,7 @@ class PriceListGenerator
     public function execute(): void
     {
         $this->createPriceList();
-        var_dump( $this->dom->saveXML() );
-
+        //var_dump( $this->dom->saveXML() );
         file_put_contents(   self::PRICE_LIST_PATH . self::PRICE_LIST_NAME, $this->dom->saveXML());
     }
 }
