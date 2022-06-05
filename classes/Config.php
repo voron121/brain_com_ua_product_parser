3d6984d7f2ceda563177654ec2dcde13fe29ae99
Вадим V <phogame121@gmail.com>
2022-06-05 14:17:18 +0300

    Fixes

--- classes/Config.php
+++ classes/Config.php
@@ -4,12 +4,12 @@ namespace App;
 
 final class Config
 {
-    public $dbHost = 'voron121.mysql.tools';
-    public $dbName = 'voron121_brain';
-    public $dbPass = 'U8y4&x6~vD';
-    public $dbUser = 'voron121_brain';
-    public $apiLogin = 'uptica85@gmail.com';
-    public $apiPassword = 'Meriel170671461';
+    public $dbHost = '';
+    public $dbName = '';
+    public $dbPass = '';
+    public $dbUser = '';
+    public $apiLogin = '';
+    public $apiPassword = '';
     public $apiEndpoint = 'http://api.brain.com.ua';
     public $apiAuthEndpoint = 'http://api.brain.com.ua/auth';
     public $shopName = 'brain';
